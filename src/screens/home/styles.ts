c0aba014f1ddf1b8c609cec821cd390f46003bf0
Aline Guiseline <alineguiseline@gmail.com>
2024-11-05 18:01:58 -0300

    creates App and Header

---
+++ src/screens/home/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.5rem; // 24px (24/16 = 1.5)
+  width: 100%;
+`;
