13091c4aa350c4d8360863fa577a57cb8f348b48
Aline Guiseline <alineguiseline@gmail.com>
2024-11-08 11:12:33 -0300

    creates Input component and InputMask

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,10 @@
+import { InputMask } from '@react-input/mask';
+
 import { Button } from '../../components/button';
+import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
-import { Filters, Header, Main, Section } from './styles';
+import { Filters, Header, InputGroup, Main, Section } from './styles';
 
 export function Home() {
   return (
@@ -17,6 +20,26 @@ export function Home() {
         <Section>
           <Filters>
             <Title title="Saldo" subtitle="Receitas e despesas no período" />
+            <InputGroup>
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Início"
+                placeholder="dd/mm/yyyy"
+              />
+              <InputMask
+                component={Input}
+                mask="dd/mm/yyyy"
+                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                variant="dark"
+                label="Fim"
+                placeholder="dd/mm/yyyy"
+              />
+              {/* É possível customizar o InputMask direto, mas é difícil, então
+              ele nos permite criar um componente de Input personalizado e importar */}
+            </InputGroup>
           </Filters>
         </Section>
       </Main>
